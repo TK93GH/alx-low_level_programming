44567dff9074d82ee3ae5545ae384a04ddb8eee0
TK93GH <tktsholo7@gmail.com>
2021-08-27 04:45:34 -0700

    redo

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints the with proper grammer, but the outcome is a piece of art
+ * Return: 0
  */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }
