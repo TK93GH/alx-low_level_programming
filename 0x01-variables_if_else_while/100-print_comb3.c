ec3cd0a1b1963b013bc37b628a4b5cfb1b6f31a6
TK93GH <tshologithub@gmail.com>
2021-08-30 12:31:49 +0200

    add space

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 	int c;
 	int d = 0;
 
-	while(d < 10)
+	while (d < 10)
 	{
 		c = 0;
 		while (c < 10)
@@ -32,5 +32,5 @@ int main(void)
 		d++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
