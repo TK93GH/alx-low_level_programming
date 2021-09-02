f8aa8415339b86ae3beff05cd4804f8e47071462
TK93GH <tshologithub@gmail.com>
2021-09-02 15:08:50 +0200

    submit all files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - Function checks for uppercase letter
+ * @c : character to check the case
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main block
+ * Description: Prints a triangle, followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
+    return (0);
+}
