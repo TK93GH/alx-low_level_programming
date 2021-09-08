a413d6d662bf41db32c5e9bbc3b37b1dbd7f7666
TK93GH <tshologithub@gmail.com>
2021-09-08 13:14:30 +0200

    fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,14 @@
  * _strlen - find the length of a string
  * @s: pointer to the string to check
  * Return: void
- */
+*/
 
 
 int _strlen(char *s)
 {
-	int i = 0;
-	while (s[i])
-		i++;
+int i = 0;
+while (s[i])
+	i++;
 
-	return (i);
+return (i);
 }
