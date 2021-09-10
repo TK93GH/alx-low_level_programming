2f7643c4af8e89889cb05dfbc04c348bc8a15aa8
TK93GH <tshologithub@gmail.com>
2021-09-10 13:52:28 +0200

    fix 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,24 @@
 #include "holberton.h"
 /**
- * _strcmp - compares two strings
- * @s1: string 1
- * @s2: string 2
- * Return: 0 if matching, and ns1 - ns2 if not matching
+* _strcmp - function being used for project 1.
+*
+* @s1: is the char 1.
+*
+* @s2: is the char 2.
+*
+* Return: - zero/NULL.
 */
 
 int _strcmp(char *s1, char *s2)
 {
-int i;
-
-/**
- * We have reached the end of S1 OR we have reached the end of S2
- * 
-*/
-
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+for ( ; *s1; )
 {
-	if (s1[i] != s2[i])
-	{
-		return (s1[i] - s2[i]);
-	}
+if (*s1 != *s2)
+{
+break;
 }
-return (0);
+s1++;
+s2++;
+}
+return (*s1 - *s2);
 }
