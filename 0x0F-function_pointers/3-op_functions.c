76d56cd86b56930278289eacdfbe50d5d6170465
TK93GH <tshologithub@gmail.com>
2021-09-29 13:53:34 +0200

    submit files

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,24 @@
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a; int b);
-int op_div(int a; int b);
-int op_mod(int a; int b);
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - select the correct operation function asked by user
+ * @s: operator argument
+ * Return: function pointer corresponding to operator given
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+	return (ops[i].f);
+}
