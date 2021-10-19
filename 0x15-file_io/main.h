4e01d0fc2f4e2d1e5095a1c1addcd3937a2f2ef8
TK93GH <tshologithub@gmail.com>
2021-10-19 09:41:46 +0200

    fix header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,4 @@
+
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
@@ -7,5 +8,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int placeholder2(void);
-
 #endif /* _MAIN_H_ */
